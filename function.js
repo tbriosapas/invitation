79d191a2ae917129a34790813e21f7cd3dbcda6b
tbriosapas <tbriosa.pas@gmail.com>
2026-02-08 12:26:21 +0800

    Delete function.js

--- function.js
+++
@@ -1,6 +0,0 @@
-function doPost(e) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1");
-  var data = JSON.parse(e.postData.contents);
-  sheet.appendRow([data.name, data.email, data.message, new Date()]);
-  return ContentService.createTextOutput("Success");
-}
